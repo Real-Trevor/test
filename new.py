2a7984a810abe7f7e18a8a0ee9005a897c99687d
Trevor Kerxhalli <trevorkerxhalli@Trevors-MacBook-Pro.local>
2022-07-18 21:53:16 -0400

    added 3 files

---
+++ new.py
@@ -0,0 +1,8 @@
+number = 15
+if number % 3 == 0:
+	print ("fizz")
+if number % 5 == 0:
+	print ("buzz")
+
+
+#fizzbuzz 5
---
+++ test.py
@@ -0,0 +1,8 @@
+#First: 10.1
+#Second: 20
+#Sum: 30.1
+
+first = input("enter your number: ")
+second = input("enter your next number: ")
+sum = float(first) + float(second)
+print("sum: " + str(sum))
